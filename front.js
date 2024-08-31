4ea9d610a53c0c44521935b4f85ded27d7aef21c
Emma Hare <173036915+emmaHare@users.noreply.github.com>
2024-08-31 16:21:17 +0200

    Connect id's to vars

--- front.js
+++ front.js
@@ -1,3 +1,28 @@
-let modal = document.getElementById('modal')
+let msgInput = document.getElementById('msg-input');
 
-modal.showModal();
+let firstModal = document.getElementById('first-modal');
+let signupModal = document.getElementById('signup-modal');
+let loginModal = document.getElementById('login-modal');
+
+let msgBtn = document.getElementById('msg-btn');
+let loginBtn = document.getElementById('login-btn');
+let signupBtn = document.getElementById('signup-btn');
+let signupSubmit = document.getElementById('signup-submit');
+
+let userInput = document.getElementById('user-input');
+let emailInput = document.getElementById('email-input');
+let pwordInput = document.getElementById('pword-input');
+let pwordConfirm = document.getElementById('pword-confirm')
+
+firstModal.showModal();
+
+loginBtn.addEventListener('click', function() {
+    loginModal.showModal();
+});
+
+signupBtn.addEventListener('click', function() {
+    signupModal.showModal();
+});
+
+
+
